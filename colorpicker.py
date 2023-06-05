801ceafa6f43b5c2588b552a4a6d38d8a34b48a2
Mustafa Öztürk <oztrk.mstf.43@gmail.com>
2023-06-05 20:41:50 +0300

    minor change

--- colorpicker.py
+++ colorpicker.py
@@ -13,7 +13,6 @@ def on_click(x, y, button, pressed):
         img = Image.new('RGB', (300, 300), color=color)
         now = datetime.now().strftime("%Y%m%d-%H%M%S")
         desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-        # Update the filename to include RGB values
         filename = f"color {r} {g} {b}.jpg"
         img.save(os.path.join(desktop_path, filename), "JPEG", quality=90)
 
